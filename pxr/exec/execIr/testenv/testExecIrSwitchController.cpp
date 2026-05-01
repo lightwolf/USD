58fbb3029ca79e63b760e51d82b2245d90a76682
adamrwoodbury <adamrwoodbury@users.noreply.github.com>
2026-04-30 17:04:43 -0700

    OpenExec: Expand testExecIrSwitchController to test specifying switch attribute values via overrides, since this is a key requirement of invertible rigging workflows.
    - Fix the IrSwitchController schema to use lowercase strings for the 'switch'
      values, since that is what the code requires.
    - Modify testExecIrSwitchController slightly to exercise the 'switch' fallback
      value.
    
    (Internal change: 2408207)

--- pxr/exec/execIr/testenv/testExecIrSwitchController.cpp
+++ pxr/exec/execIr/testenv/testExecIrSwitchController.cpp
@@ -43,6 +43,19 @@ PXR_NAMESPACE_USING_DIRECTIVE
         }                                                                      \
     }()
 
+#define ASSERT_EQ(expr, expected)                                              \
+    [&] {                                                                      \
+        auto&& expr_ = expr;                                                   \
+        if (expr_ != expected) {                                               \
+            std::cout << std::flush;                                           \
+            std::cerr << std::flush;                                           \
+            TF_FATAL_ERROR(                                                    \
+                "Expected " TF_PP_STRINGIZE(expr) " == '%s'; got '%s'",        \
+                TfStringify(expected).c_str(),                                 \
+                TfStringify(expr_).c_str());                                   \
+        }                                                                      \
+    }()
+
 struct _EnsureNoErrors {
     ~_EnsureNoErrors() {
         TF_VERIFY(mark.IsClean());
@@ -51,14 +64,17 @@ struct _EnsureNoErrors {
     TfErrorMark mark;
 };
 
-// Tests inverse and forward computation by:
-// - Solving for input avar values with the given desired values as goals for
-//   the output spaces and confirming we get the expected input values.
-// - Authoring the resulting input values onto the input avars and computing
-//   the output spaces and confirming that we get the desired values that were
-//   used for the initial inversion.
+// Tests inverse and forward computation through a switch controller by:
+// - Solving for input avar values with a switch avar override that selects a
+//   specified rig and the given desired values as goals for the output spaces,
+//   to confirm we get the expected input values.
+// - Authoring the switch avar value and authoring the resulting input values
+//   onto the input avars and computing the output spaces, to confirm that we
+//   get the desired values that were used as goals for the initial inversion.
 static
-void _VerifyInverseAndForwardResults(
+void _VerifySwitchInverseAndForwardResults(
+    const UsdAttribute &switchAttribute,
+    const TfToken &switchValue,
     const std::vector<UsdPrim> &inputPrims,
     const std::vector<double> &expectedInputValues,
     const std::vector<UsdAttribute> &outputSpaces,
@@ -66,6 +82,12 @@ void _VerifyInverseAndForwardResults(
     ExecUsdSystem *execSystem)
 {
     TF_AXIOM(outputSpaces.size() == desiredValues.size());
+
+    // Verify that the switch value isn't what the switch is currently set to,
+    // to make sure the switch value override has an effect.
+    VtValue value;
+    TF_AXIOM(switchAttribute.Get(&value));
+    TF_AXIOM(value != switchValue);
 
     std::vector<UsdAttribute> inputAvars;
     for (const UsdPrim &prim : inputPrims) {
@@ -93,6 +115,10 @@ void _VerifyInverseAndForwardResults(
         TF_AXIOM(request.IsValid());
 
         ExecUsdValueOverrideVector overrides;
+        overrides.push_back(
+            {{switchAttribute,
+              ExecBuiltinComputations->computeValue},
+             VtValue(switchValue)});
         for (size_t i=0; i<desiredValues.size(); ++i) {
             overrides.push_back(
                 {{outputSpaces[i],
@@ -109,13 +135,14 @@ void _VerifyInverseAndForwardResults(
         }
     }
 
-    // Author the expected input values and do a forward compute to confirm we
-    // get the desired output values.
-    {
-        for (unsigned int i=0; i<expectedInputValues.size(); ++i) {
-            inputAvars[i].Set(expectedInputValues[i]);
-        }
+    // Author the switch value and the expected input values and do a forward
+    // compute to confirm we get the desired output values.
+    switchAttribute.Set(switchValue);
+    for (unsigned int i=0; i<expectedInputValues.size(); ++i) {
+        inputAvars[i].Set(expectedInputValues[i]);
+    }
 
+    {
         std::vector<ExecUsdValueKey> valueKeys;
         for (const UsdAttribute &attr : outputSpaces) {
             valueKeys.emplace_back(attr, ExecBuiltinComputations->computeValue);
@@ -194,10 +221,9 @@ Test_BasicSwitch()
         usdStage->GetAttributeAtPath(SdfPath("/Root/Switch.switch"));
     TF_AXIOM(switchAttr);
 
-    // Set the switch controller so Rig1 is active and compute in the forward
-    // direction and confirm that we get the values Rig1 computes as authored.
-    switchAttr.Set(ExecIrSwitchControllerTokens->rig1);
-
+    // With Rig1 active ('rig1' is the fallback value for the 'switch' avar),
+    // compute in the forward direction and confirm that we get the values Rig1
+    // computes as authored.
     {
         ExecUsdCacheView cache = execSystem.Compute(outputRequest);
         ASSERT_CLOSE(
@@ -222,7 +248,10 @@ Test_BasicSwitch()
             []{ GfMatrix4d mat; mat.SetTranslate({0, 4, 0}); return mat; }());
     }
 
-    // With the switch still set to Rig2, test inverse computation.
+    // Set the switch to Rig1 and test inverse computation with an override that
+    // selects Rig2.
+    switchAttr.Set(ExecIrSwitchControllerTokens->rig1);
+
     {
         const UsdPrim fk1 = usdStage->GetPrimAtPath(SdfPath("/Root/Rig2/FK1"));
         const UsdPrim fk2 = usdStage->GetPrimAtPath(SdfPath("/Root/Rig2/FK2"));
@@ -240,7 +269,8 @@ Test_BasicSwitch()
              0.0, 0.0, 0.0, 0.0,    10.0, 20.0, 30.0,
         };
 
-        _VerifyInverseAndForwardResults(
+        _VerifySwitchInverseAndForwardResults(
+            switchAttr, ExecIrSwitchControllerTokens->rig2,
             {fk1, fk2},
             expectedInputValues,
             {joint1Space, joint2Space},
@@ -248,10 +278,13 @@ Test_BasicSwitch()
             &execSystem);
     }
 
-    // Set the switch to Rig1 and test inverse computation again.
-    switchAttr.Set(ExecIrSwitchControllerTokens->rig1);
-
+    // Verify the switch is now set to Rig2 and test inverse with an override
+    // that selects Rig1.
     {
+        VtValue value;
+        TF_AXIOM(switchAttr.Get(&value));
+        ASSERT_EQ(value, ExecIrSwitchControllerTokens->rig2);
+
         const UsdPrim fk1 = usdStage->GetPrimAtPath(SdfPath("/Root/Rig1/FK1"));
         const UsdPrim fk2 = usdStage->GetPrimAtPath(SdfPath("/Root/Rig1/FK2"));
         TF_AXIOM(fk1 && fk2);
@@ -268,7 +301,8 @@ Test_BasicSwitch()
             0.0, -90.0, 0.0, 0.0,    0.0, 0.0, 0.0,
         };
 
-        _VerifyInverseAndForwardResults(
+        _VerifySwitchInverseAndForwardResults(
+            switchAttr, ExecIrSwitchControllerTokens->rig1,
             {fk1, fk2},
             expectedInputValues,
             {joint1Space, joint2Space},
